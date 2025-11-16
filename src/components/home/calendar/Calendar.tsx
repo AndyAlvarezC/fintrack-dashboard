109706a12692381ac5aa9d30372f322294fb0fb2
CHAIK70 <andykanka@gmail.com>
2025-11-16 01:23:23 +0100

    Add custom calendar

--- src/components/home/calendar/Calendar.tsx
+++ src/components/home/calendar/Calendar.tsx
@@ -1,9 +1,15 @@
+import CalendarPicker from "./components/CalendarPicker"
+
 export default function Calendar() {
   return (
     <div className="flex flex-col w-full h-full">
       {/* Header */}
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-md font-semibold">Calendar</h2>
+      </div>
+
+      <div className="w-full">
+        <CalendarPicker />
       </div>
     </div>
   )
